00413656a6dca1f5b849daca8b897048a29cf646
theXodus <jprice1990@gmail.com>
2016-05-04 13:45:38 -0700

    Rspec files added

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  it "has a valid factory" do
+    item = FactoryGirl.create(:item)
+    expect(item).to be_valid
+  end
+
+  it "is invalid without a name" do
+    item = Item.create(name: nil)
+    expect(item).not_to be_valid
+  end
+
+
 
 end
